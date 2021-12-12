26dd4eaecaad7fdb87adebf0b0444d1700a17165
nico <pfunes@fi.uba.ar>
2021-12-12 15:04:02 -0300

    add endpoints for list, create, update and delete device

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,40 +1,91 @@
 //=======[ Settings, Imports & Data ]==========================================
 
-var PORT    = 3000;
+var PORT = 3000;
 
-var express = require('express');
-var app     = express();
-var utils   = require('./mysql-connector');
+var express = require("express");
+var app = express();
+var utils = require("./mysql-connector");
 
 // to parse application/json
-app.use(express.json()); 
+app.use(express.json());
 // to serve static files
-app.use(express.static('/home/node/app/static/'));
+app.use(express.static("/home/node/app/static/"));
 
 //=======[ Main module code ]==================================================
 
-app.get('/devices/', function(req, res, next) {
-    devices = [
-        { 
-            'id': 1, 
-            'name': 'Lampara 1', 
-            'description': 'Luz living', 
-            'state': 0, 
-            'type': 1, 
-        },
-        { 
-            'id': 2, 
-            'name': 'Ventilador 1', 
-            'description': 'Ventilador Habitacion', 
-            'state': 1, 
-            'type': 2, 
-        },
-    ]
-    res.send(JSON.stringify(devices)).status(200);
+// Endpoit to list all the devices
+app.get("/devices/", function (req, res, next) {
+  utils.query("SELECT * FROM Devices", function (error, results, fields) {
+    if (error) {
+      console.log(error);
+      res
+        .status(500)
+        .send({ error: "Ha ocurrido un error, intente nuevamente más tarde." });
+      return;
+    }
+    res.send(results);
+  });
 });
 
-app.listen(PORT, function(req, res) {
-    console.log("NodeJS API running correctly");
+//Endpoint to create a new device, the device starts with status 0
+
+app.post("/devices/", function (req, res, next) {
+  utils.query(
+    "INSERT INTO Devices (id, name, description,state, type) VALUES (?,?,?,0,?)",
+    [req.body.id, req.body.name, req.body.description, req.body.type],
+    function (error, results, fields) {
+      if (error) {
+        console.log(error);
+        res.status(500).send({
+          error: "Ha ocurrido un error, intente nuevamente más tarde.",
+        });
+        return;
+      }
+      res.status(201).end();
+    }
+  );
+});
+
+//Endpoint to create a new device, the device starts with status 0
+
+app.put("/devices/", function (req, res, next) {
+  utils.query(
+    "UPDATE Devices SET name = ?, description = ?, state = ?, type = ?  WHERE id = ? ",
+    [req.body.name, req.body.description, req.body.state, req.body.type, req.body.id],
+    function (error, results, fields) {
+      if (error) {
+        console.log(error);
+        res.status(500).send({
+          error: "Ha ocurrido un error, intente nuevamente más tarde.",
+        });
+        return;
+      }
+      res.status(200).end();
+    }
+  );
+});
+
+//Endpoint to delete a device
+
+app.delete("/devices/:id", function (req, res, next) {
+    utils.query(
+      "DELETE from Devices WHERE id = ? ",
+      [req.params.id],
+      function (error, results, fields) {
+        if (error) {
+          console.log(error);
+          res.status(500).send({
+            error: "Ha ocurrido un error, intente nuevamente más tarde.",
+          });
+          return;
+        }
+        res.status(200).send({message: "Se ha eliminado el device correctamente"});
+      }
+    );
+  });
+
+app.listen(PORT, function (req, res) {
+  console.log("NodeJS API running correctly");
 });
 
 //=======[ End of file ]=======================================================

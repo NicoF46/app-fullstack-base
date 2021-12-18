c3fcc747f6b7cc5ebc5ed83ef0079b193668ab76
nico <pfunes@fi.uba.ar>
2021-12-18 20:26:35 -0300

    refactor code and add display options

--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -4,12 +4,14 @@ class Main implements EventListenerObject, RestResponseLister {
   private deviceList: Array<Device> = new Array();
   private framework: FrameWork = new FrameWork();
   private currentDevice: Device;
+  private showOption: String;
 
   constructor() {
     this.list_devices();
+    this.showOption = "list";
   }
 
-// Method to handle the UI actions
+  // Method to handle the UI actions
   public handleEvent(ev: Event) {
     let objetoEvento = <HTMLElement>ev.target;
     if (ev.type == "click") {
@@ -19,22 +21,28 @@ class Main implements EventListenerObject, RestResponseLister {
         this.add_device();
       } else if (objetoEvento.id == "submit_btn_update") {
         this.update_device(objetoEvento.id);
-      }
-      else if(objetoEvento.id.includes("update_")){
-        this.currentDevice = this.deviceList.find((e) => e.id === Number(objetoEvento.id.split("_").pop()));
-        
+      } else if (objetoEvento.id.includes("update_")) {
+        this.currentDevice = this.deviceList.find(
+          (e) => e.id === Number(objetoEvento.id.split("_").pop())
+        );
+      } else if (objetoEvento.id == "icon_btn") {
+        this.showOption = "icon";
+        this.list_devices();
+      } else if (objetoEvento.id == "list_btn") {
+        this.showOption = "list";
+        this.list_devices();
       }
     } else if (ev.type == "change" && objetoEvento.id.includes("bar")) {
       this.set_device_value(objetoEvento.id);
     }
   }
 
-// Method to fetch the id of an html element
+  // Method to fetch the id of an html element
   public getElement(id: string): HTMLElement {
     return document.getElementById(id);
   }
 
-// Method to request the addition of an new element to the backend
+  // Method to request the addition of an new element to the backend
   public add_device() {
     let requestBody = {
       id: this.getElement("device_id")["value"],
@@ -49,7 +57,7 @@ class Main implements EventListenerObject, RestResponseLister {
     );
   }
 
-// Method to request the deletion of an element to the backend
+  // Method to request the deletion of an element to the backend
   public delete_device(id: string) {
     let deviceId = id.split("_").pop();
     this.framework.requestDELETE(
@@ -58,12 +66,12 @@ class Main implements EventListenerObject, RestResponseLister {
     );
   }
 
-// Method to request the list of devices from the backend
+  // Method to request the list of devices from the backend
   public list_devices() {
     this.framework.requestGET("http://localhost:8000/devices", this);
   }
 
-//Method to request the update of the state value of a device to the backend
+  //Method to request the update of the state value of a device to the backend
   public set_device_value(id: string) {
     let deviceId = id.split("_").pop();
     let deviceData = this.deviceList.find((e) => e.id === Number(deviceId));
@@ -81,7 +89,7 @@ class Main implements EventListenerObject, RestResponseLister {
     );
   }
 
-//Method to request the update of a device to the backend
+  //Method to request the update of a device to the backend
   public update_device(id: string) {
     let requestBody = {
       id: this.currentDevice.id,
@@ -97,14 +105,41 @@ class Main implements EventListenerObject, RestResponseLister {
     );
   }
 
-//Method to handle the response from the GET response 
+  //Method to handle the response from the GET response
   public handlerGetResponse(status: number, response: string) {
     let devices: Array<Device> = JSON.parse(response);
     this.deviceList = devices;
     let devices_list = this.getElement("devices_list");
     devices_list.innerHTML = "";
     for (let device of devices) {
-      devices_list.innerHTML += `<li class="collection-item avatar">
+      if (this.showOption == "icon") {
+        devices_list.innerHTML += ` <div class="card col s4 m4 l4 xl4">
+      <div class="card-image waves-effect waves-block waves-light">
+        <img class="activator" src="static/images/robot.jpg">
+      </div>
+      <div class="card-content" col s2 m2 l2 xl2>
+        <span class="card-title activator grey-text text-darken-4">${device.name}<i class="material-icons right">more_vert</i></span>
+        <button class="btn waves-effect waves-light button-view" name="delete_button" id=delete_btn_${device.id}>
+        <i class="small material-icons white-text">delete</i>
+    </button>
+    <button class="btn waves-effect waves-light button-view modal-trigger" name="update_button" id=update_${device.id} data-toggle='modal_${device.id}' data-target="update_modal">
+       <i class="small material-icons white-text">edit</i>
+    </button>
+    <a href="#!" class="secondary-content">
+          <p class="range-field">
+            <input type="range" id="bar_${device.id}" min="0" max="100" value=${device.state} />
+          </p>
+        </a>
+      </div>
+      <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4">${device.name}<i class="material-icons right">close</i></span>
+        <p>${device.description}.</p>
+        <p>Type: ${device.type}.</p>
+        <p>State: ${device.state}.</p>
+      </div>
+    </div>`;
+      } else
+        devices_list.innerHTML += `<li class="collection-item avatar">
         <img src="static/images/robot.jpg" alt="" class="circle">
         <span class="title">${device.name}</span>
         <p>${device.description} <br>
@@ -133,17 +168,17 @@ class Main implements EventListenerObject, RestResponseLister {
     }
   }
 
-//Method to handle the response from the DELETE response 
+  //Method to handle the response from the DELETE response
   public handlerDeleteResponse(status: number, response: string) {
     this.list_devices();
   }
 
-//Method to handle the response from the POST response 
+  //Method to handle the response from the POST response
   public handlerPostResponse(status: number, response: string) {
     this.list_devices();
   }
 
-//Method to handle the response from the PUT response 
+  //Method to handle the response from the PUT response
   public handlerPutResponse(status: number, response: string) {
     this.list_devices();
   }
@@ -160,4 +195,8 @@ window.onload = function inicio() {
   submitBtn.addEventListener("click", miObjeto);
   let submitBtnUpdate = miObjeto.getElement("submit_btn_update");
   submitBtnUpdate.addEventListener("click", miObjeto);
+  let iconBtn = miObjeto.getElement("icon_btn");
+  iconBtn.addEventListener("click", miObjeto);
+  let listBtn = miObjeto.getElement("list_btn");
+  listBtn.addEventListener("click", miObjeto);
 };
